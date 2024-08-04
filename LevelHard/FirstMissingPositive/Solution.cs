7991fc6089122fd9475b43516e8694a66bf3c0af
rkanwal593 <rabiakanwal593@gmail.com>
2024-08-04 17:59:29 +0200

    First missing positive solution beats 81.5 %

---
+++ LevelHard/FirstMissingPositive/Solution.cs
@@ -0,0 +1,25 @@
+public class Solution {
+    public int FirstMissingPositive(int[] nums) {
+        int n = nums.Length;
+        
+        for (int i = 0; i < n; i++) {
+            while (nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] != nums[i]) {
+                Swap(nums, i, nums[i] - 1);
+            }
+        }
+        
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != i + 1) {
+                return i + 1;
+            }
+        }
+        
+        return n + 1;
+    }
+    
+    private void Swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
