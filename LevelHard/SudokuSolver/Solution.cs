a7c21329ea115afa4b87b3ec59b7e9d43a0b0779
rkanwal593 <rabiakanwal593@gmail.com>
2024-08-04 18:03:44 +0200

    Sudoku Solver Hash table C#
    beats 60.5% , the solution uses backtracking technique

---
+++ LevelHard/SudokuSolver/Solution.cs
@@ -0,0 +1,35 @@
+public class Solution {
+    public void SolveSudoku(char[][] board) {
+        Solve(board);
+    }
+    
+    private bool Solve(char[][] board) {
+        for (int row = 0; row < 9; row++) {
+            for (int col = 0; col < 9; col++) {
+                if (board[row][col] == '.') {
+                    for (char num = '1'; num <= '9'; num++) {
+                        if (IsValid(board, row, col, num)) {
+                            board[row][col] = num;
+                            if (Solve(board)) {
+                                return true;
+                            }
+                            board[row][col] = '.';
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    
+    private bool IsValid(char[][] board, int row, int col, char num) {
+        for (int i = 0; i < 9; i++) {
+            if (board[row][i] == num || board[i][col] == num || 
+                board[row / 3 * 3 + i / 3][col / 3 * 3 + i % 3] == num) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
