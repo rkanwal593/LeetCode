f27f9e0e9e9a21b8c5555a7af62a8e27a60ce8bf
rkanwal593 <rabiakanwal593@gmail.com>
2024-08-04 18:10:14 +0200

    4Sum

---
+++ LevelMedium/4Sum/Solution.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+
+public class Solution {
+    public IList<IList<int>> FourSum(int[] nums, int target) {
+        Array.Sort(nums);
+        IList<IList<int>> result = new List<IList<int>>();
+        int n = nums.Length;
+        
+        for (int i = 0; i < n - 3; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue; // skip duplicates
+            
+            for (int j = i + 1; j < n - 2; j++) {
+                if (j > i + 1 && nums[j] == nums[j - 1]) continue; // skip duplicates
+                
+                int left = j + 1;
+                int right = n - 1;
+                
+                while (left < right) {
+                    long sum = (long)nums[i] + nums[j] + nums[left] + nums[right];
+                    
+                    if (sum == target) {
+                        result.Add(new List<int> { nums[i], nums[j], nums[left], nums[right] });
+                        
+                        while (left < right && nums[left] == nums[left + 1]) left++; // skip duplicates
+                        while (left < right && nums[right] == nums[right - 1]) right--; // skip duplicates
+                        
+                        left++;
+                        right--;
+                    } else if (sum < target) {
+                        left++;
+                    } else {
+                        right--;
+                    }
+                }
+            }
+        }
+        
+        return result;
+    }
+}
